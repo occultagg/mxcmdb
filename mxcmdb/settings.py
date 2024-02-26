8b31da3716b19770129fa7196d842a5352ab8c32
occultagg <107286777+occultagg@users.noreply.github.com>
2024-02-26 12:21:31 +0800

    Update settings.py

--- mxcmdb/settings.py
+++ mxcmdb/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mxcmdb',
         'USER': 'root',
-        'PASSWORD': '7705My))',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
